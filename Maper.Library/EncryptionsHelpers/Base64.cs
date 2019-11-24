cec16de9c507a4987ee2d824ce5490c047dc5560
Mario Oliveira <maper765@hotmail.com>
2019-11-23 21:30:13 -0300

    Start

---
+++ Maper.Library/EncryptionsHelpers/Base64.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Maper.Library.EncryptionsHelpers 
+{
+    public class Base64 : IBase64
+    {
+        public string Encrypt(string value)
+            => Convert.ToBase64String(Encoding.ASCII.GetBytes(value));
+
+        public string Decrypt(string value)
+            => Encoding.ASCII.GetString(Convert.FromBase64String(value));
+
+
+        public string EncryptUTF8(string value)
+            => Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+
+        public string DecryptUTF8(string value)
+            => Encoding.UTF8.GetString(Convert.FromBase64String(value));
+    }
+}
---
+++ Maper.Library/EncryptionsHelpers/IBase64.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Maper.Library.EncryptionsHelpers
+{
+    public interface IBase64 
+    {
+        string Encrypt(string texto);
+
+        string Decrypt(string texto);
+
+        string EncryptUTF8(string texto);
+
+        string DecryptUTF8(string texto);
+    }
+}
